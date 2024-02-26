ce175126f6bd01720274abc712ff5c07fde1c34a
hiewie <hielkow@hotmail.com>
2024-02-26 10:05:32 +0100

    Update

--- dsnd.io/Program.cs
+++ dsnd.io/Program.cs
@@ -7,21 +7,38 @@ if (args.Length > 0)
     var arg0 = args[0];
     if (arg0.Contains('*') || arg0.Contains('?'))
     {
-        foreach (var file in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), arg0, new EnumerationOptions { RecurseSubdirectories = true }))
+        var files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), arg0, new EnumerationOptions { RecurseSubdirectories = true });
+
+        foreach (var filename in files)
         {
-            var dsndSound = new DSND.RiffParser().ParseDsndFile(new FileInfo(file));
-            new Export().ExportSamples(null, dsndSound);
+            var fi  = new FileInfo(filename);
+            var dsndSound = new RiffParser().ParseDsndFile(fi);
+            Console.WriteLine($"Exporting {filename}");
+
+            var pathstr = fi.DirectoryName;
+            var p = pathstr.LastIndexOf(Path.DirectorySeparatorChar);
+            pathstr = p>0? pathstr.Substring(p+1): pathstr; 
+            var path = new DirectoryInfo(pathstr+"_Export");
+            new Export().ExportSamples(path, dsndSound);
         }
+
+
+        // Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = 2 }, filename =>
+        //{
+        //    var dsndSound = new RiffParser().ParseDsndFile(new FileInfo(filename));
+        //    Console.WriteLine($"Exporting {filename}");
+        //    new Export().ExportSamples(null, dsndSound);
+        //});
     }
     else
     {
         var fileInfo = new FileInfo(args[0]);
-
-        var dsndSound = new DSND.RiffParser().ParseDsndFile(fileInfo);
-
+        var dsndSound = new RiffParser().ParseDsndFile(fileInfo);
+        Console.WriteLine($"Exporting {fileInfo.Name}");
         new Export().ExportSamples(null, dsndSound);
     }
-} else
+}
+else
 {
     Console.WriteLine("Usage: dsnd.io.exe filename or dsnd.io.exe wildcard, *.* for example");
 }
