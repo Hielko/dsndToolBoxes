c1dd9ed317177950e7222e6aa6962bdf5280f88b
hiewie <hielkow@hotmail.com>
2024-02-27 14:13:52 +0100

    Better dir handling

--- dsnd.io/Program.cs
+++ dsnd.io/Program.cs
@@ -4,10 +4,9 @@ Console.WriteLine("Export DSND file(s)");
 
 if (args.Length > 0)
 {
+    var currentDirectory = Directory.GetCurrentDirectory();
     var argOptions = new GetOpt(args);
-
     var recurseSubdirectories = argOptions.ExistValue("-r");
-
     var overwriteExistingFiles = argOptions.ExistValue("-o");
 
     int tasks = 1;
@@ -22,34 +21,70 @@ if (args.Length > 0)
         }
     }
 
+    DirectoryInfo exportDirectory = null;
+    if (argOptions.TryGetValue("-e", out var exportDirectoryStr))
+    {
+        exportDirectory = new DirectoryInfo(exportDirectoryStr);
+    }
+    else
+    {
+        exportDirectory = new DirectoryInfo("Export"); // + Path.DirectorySeparatorChar + "Export");
+    }
+
+
     var arg0 = args[0];
+    Console.WriteLine($"Directory: {arg0}");
+
     IEnumerable<string> files;
     if (arg0.Contains('*') || arg0.Contains('?'))
     {
-        files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), arg0, new EnumerationOptions { RecurseSubdirectories = recurseSubdirectories });
+        var p = arg0.LastIndexOf(Path.DirectorySeparatorChar);
+        var searchDirectory = p != -1 ? arg0.Substring(0, p) : arg0;
+        var pattern = p != -1 ? arg0.Substring(p + 1) : arg0;
+
+        if (!(new DirectoryInfo(searchDirectory).Exists)) {
+            searchDirectory = currentDirectory;
+        }
+
+        Console.WriteLine($"searchDirectory {searchDirectory}, pattern {pattern}");
+        files = Directory.EnumerateFiles(searchDirectory, pattern, new EnumerationOptions { RecurseSubdirectories = recurseSubdirectories });
     }
     else
     {
         files = new List<string>() { arg0 };
     }
 
+    Console.WriteLine($"Files: {files.Count()}");
+
     Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = tasks }, filename =>
     {
         var fi = new FileInfo(filename);
+
         if (!fi.Exists)
         {
             Console.WriteLine($"{filename} does not exists.");
         }
         if (fi.Extension.ToLower() == ".dsnd")
         {
+            // Strip drive
+            var tmp = fi.DirectoryName;
+            while (tmp[0] != Path.DirectorySeparatorChar)
+            {
+                tmp = tmp.Remove(0, 1);
+            }
+
             var dsndSound = new RiffParser().ParseDsndFile(fi);
             Console.WriteLine($"Exporting {filename}");
 
             var pathstr = fi.DirectoryName;
             var p = pathstr.LastIndexOf(Path.DirectorySeparatorChar);
             pathstr = p > 0 ? pathstr.Substring(p + 1) : pathstr;
-            var path = new DirectoryInfo(pathstr + "_Export");
-            new Export().ExportSamples(path, dsndSound);
+            var path = new DirectoryInfo(exportDirectory.FullName + tmp + Path.DirectorySeparatorChar + pathstr + "_Export");
+
+            Console.WriteLine($"Exporting {filename} to {path}");
+
+
+            new Export().ExportSamples(path, dsndSound, overwriteExistingFiles);
         }
     });
 
@@ -60,4 +95,6 @@ else
     Console.WriteLine("Usage: dsnd.io.exe filename or dsnd.io.exe wildcard, *.* for example");
     Console.WriteLine("Options:  -t <n> where n is number of max threads, minimal 2 when present");
     Console.WriteLine("          -r recursive");
+    Console.WriteLine("          -o overwrite existing files");
+    Console.WriteLine("          -e export directory");
 }
