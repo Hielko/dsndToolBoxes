0cb43ec44912ec3c67eea03a3aca830364408804
hiewie <hielkow@hotmail.com>
2024-02-27 10:01:21 +0100

    Added options and fixes

---
+++ dsnd.io/GetOpt.cs
@@ -0,0 +1,44 @@
+﻿using System.Formats.Asn1;
+
+namespace DSND
+{
+    public class GetOpt
+    {
+        readonly string[] args;
+
+        public GetOpt(string[] args)
+        {
+            this.args = args;
+        }
+
+
+        public bool ExistValue(string tag)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == tag) return true;
+
+            }
+            return false;
+        }
+
+
+        public bool TryGetValue(string tag, out string? value)
+        {
+            value = null;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == tag && i + 1 < args.Length)
+                {
+                    if (!String.IsNullOrEmpty(args[i + 1]))
+                    {
+                        value = args[i + 1];
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
--- dsnd.io/Program.cs
+++ dsnd.io/Program.cs
@@ -4,41 +4,60 @@ Console.WriteLine("Export DSND file(s)");
 
 if (args.Length > 0)
 {
+    var argOptions = new GetOpt(args);
+
+    var recurseSubdirectories = argOptions.ExistValue("-r");
+
+    var overwriteExistingFiles = argOptions.ExistValue("-o");
+
+    int tasks = 1;
+    if (argOptions.TryGetValue("-t", out var tasksStr))
+    {
+        if (int.TryParse(tasksStr, out tasks))
+        {
+            if (tasks < 2)
+                throw new Exception("tasks must be absent or > 1");
+            if (tasks > 16)
+                throw new Exception("tasks cant be more than 16");
+        }
+    }
+
     var arg0 = args[0];
+    IEnumerable<string> files;
     if (arg0.Contains('*') || arg0.Contains('?'))
     {
-        var files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), arg0, new EnumerationOptions { RecurseSubdirectories = true });
+        files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), arg0, new EnumerationOptions { RecurseSubdirectories = recurseSubdirectories });
+    }
+    else
+    {
+        files = new List<string>() { arg0 };
+    }
 
-        foreach (var filename in files)
+    Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = tasks }, filename =>
+    {
+        var fi = new FileInfo(filename);
+        if (!fi.Exists)
         {
-            var fi  = new FileInfo(filename);
+            Console.WriteLine($"{filename} does not exists.");
+        }
+        if (fi.Extension.ToLower() == ".dsnd")
+        {
             var dsndSound = new RiffParser().ParseDsndFile(fi);
             Console.WriteLine($"Exporting {filename}");
 
             var pathstr = fi.DirectoryName;
             var p = pathstr.LastIndexOf(Path.DirectorySeparatorChar);
-            pathstr = p>0? pathstr.Substring(p+1): pathstr; 
-            var path = new DirectoryInfo(pathstr+"_Export");
+            pathstr = p > 0 ? pathstr.Substring(p + 1) : pathstr;
+            var path = new DirectoryInfo(pathstr + "_Export");
             new Export().ExportSamples(path, dsndSound);
         }
+    });
 
-
-        // Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = 2 }, filename =>
-        //{
-        //    var dsndSound = new RiffParser().ParseDsndFile(new FileInfo(filename));
-        //    Console.WriteLine($"Exporting {filename}");
-        //    new Export().ExportSamples(null, dsndSound);
-        //});
-    }
-    else
-    {
-        var fileInfo = new FileInfo(args[0]);
-        var dsndSound = new RiffParser().ParseDsndFile(fileInfo);
-        Console.WriteLine($"Exporting {fileInfo.Name}");
-        new Export().ExportSamples(null, dsndSound);
-    }
 }
+
 else
 {
     Console.WriteLine("Usage: dsnd.io.exe filename or dsnd.io.exe wildcard, *.* for example");
+    Console.WriteLine("Options:  -t <n> where n is number of max threads, minimal 2 when present");
+    Console.WriteLine("          -r recursive");
 }
